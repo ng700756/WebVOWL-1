f3e3a88434622bc38eb5bea57906e6b7541c89ab
Lim <lim@iMac-de-CONT01DK1004.local>
2016-10-26 14:08:39 -0500

    main div expand

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -5,26 +5,29 @@ module.exports = function () {
 		options = graph.graphOptions(),
 		languageTools = webvowl.util.languageTools(),
 		GRAPH_SELECTOR = "#graph",
-	// Modules for the webvowl app
-		exportMenu = require("./menu/exportMenu")(graph),
-		filterMenu = require("./menu/filterMenu")(graph),
-		gravityMenu = require("./menu/gravityMenu")(graph),
-		modeMenu = require("./menu/modeMenu")(graph),
+
 		ontologyMenu = require("./menu/ontologyMenu")(graph),
-		pauseMenu = require("./menu/pauseMenu")(graph),
-		resetMenu = require("./menu/resetMenu")(graph),
-		sidebar = require("./sidebar")(graph),
-	// Graph modules
+
+		// Modules for the webvowl app
+		/*
+		 exportMenu = require("./menu/exportMenu")(graph),
+		 filterMenu = require("./menu/filterMenu")(graph),
+		 gravityMenu = require("./menu/gravityMenu")(graph),
+		 modeMenu = require("./menu/modeMenu")(graph),
+		 ontologyMenu = require("./menu/ontologyMenu")(graph),
+		 pauseMenu = require("./menu/pauseMenu")(graph),
+		 resetMenu = require("./menu/resetMenu")(graph),*/
+		//sidebar = require("./sidebar")(graph),
+		// Graph modules
 		colorExternalsSwitch = webvowl.modules.colorExternalsSwitch(graph),
 		compactNotationSwitch = webvowl.modules.compactNotationSwitch(graph),
 		datatypeFilter = webvowl.modules.datatypeFilter(),
 		disjointFilter = webvowl.modules.disjointFilter(),
 		focuser = webvowl.modules.focuser(),
-		nodeDegreeFilter = webvowl.modules.nodeDegreeFilter(filterMenu),
 		nodeScalingSwitch = webvowl.modules.nodeScalingSwitch(graph),
 		objectPropertyFilter = webvowl.modules.objectPropertyFilter(),
 		pickAndPin = webvowl.modules.pickAndPin(),
-		selectionDetailDisplayer = webvowl.modules.selectionDetailsDisplayer(sidebar.updateSelectionInformation),
+		//selectionDetailDisplayer = webvowl.modules.selectionDetailsDisplayer(sidebar.updateSelectionInformation),
 		statistics = webvowl.modules.statistics(),
 		subclassFilter = webvowl.modules.subclassFilter(),
 		setOperatorFilter = webvowl.modules.setOperatorFilter();
@@ -32,7 +35,7 @@ module.exports = function () {
 	app.initialize = function () {
 		options.graphContainerSelector(GRAPH_SELECTOR);
 		options.selectionModules().push(focuser);
-		options.selectionModules().push(selectionDetailDisplayer);
+		//options.selectionModules().push(selectionDetailDisplayer);
 		options.selectionModules().push(pickAndPin);
 		options.filterModules().push(statistics);
 		options.filterModules().push(datatypeFilter);
@@ -41,27 +44,25 @@ module.exports = function () {
 		options.filterModules().push(disjointFilter);
 		options.filterModules().push(setOperatorFilter);
 		options.filterModules().push(nodeScalingSwitch);
-		options.filterModules().push(nodeDegreeFilter);
+
 		options.filterModules().push(compactNotationSwitch);
 		options.filterModules().push(colorExternalsSwitch);
 
 		d3.select(window).on("resize", adjustSize);
 
-		exportMenu.setup();
-		gravityMenu.setup();
-		filterMenu.setup(datatypeFilter, objectPropertyFilter, subclassFilter, disjointFilter, setOperatorFilter, nodeDegreeFilter);
-		modeMenu.setup(pickAndPin, nodeScalingSwitch, compactNotationSwitch, colorExternalsSwitch);
-		pauseMenu.setup();
-		sidebar.setup();
+
 		ontologyMenu.setup(loadOntologyFromText);
-		resetMenu.setup([gravityMenu, filterMenu, modeMenu, focuser, selectionDetailDisplayer, pauseMenu]);
+
+		//resetMenu.setup([gravityMenu, filterMenu, modeMenu, focuser, pauseMenu]);
+
+
 
 		graph.start();
 		adjustSize();
 	};
 
 	function loadOntologyFromText(jsonText, filename, alternativeFilename) {
-		pauseMenu.reset();
+		//pauseMenu.reset();
 
 		var data;
 		if (jsonText) {
@@ -80,24 +81,25 @@ module.exports = function () {
 			}
 		}
 
-		exportMenu.setJsonText(jsonText);
+		//exportMenu.setJsonText(jsonText);
 
 		options.data(data);
 		graph.reload();
-		sidebar.updateOntologyInformation(data, statistics);
+		//sidebar.updateOntologyInformation(data, statistics);
 
-		exportMenu.setFilename(filename);
+		//exportMenu.setFilename(filename);
 	}
 
 	function adjustSize() {
 		var graphContainer = d3.select(GRAPH_SELECTOR),
 			svg = graphContainer.select("svg"),
-			height = window.innerHeight - 40,
-			width = window.innerWidth - (window.innerWidth * 0.22);
+			height = window.innerHeight,
+			width = window.innerWidth;
 
-		graphContainer.style("height", height + "px");
+		graphContainer.style("width", width + "px");
+
 		svg.attr("width", width)
-			.attr("height", height);
+			.attr("height", height );
 
 		options.width(width)
 			.height(height);
